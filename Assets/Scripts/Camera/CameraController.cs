c46551dc817e2fe7f769402418ce5e1406c91dd6
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-24 11:00:06 -0400

    added movement, jump, camera follow

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float xMax;
+    public float yMax;
+    public float xMin;
+    public float yMin;
+
+    public Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    float horizontal;
+
+    public float jumpForce;
+    public float movementSpeed;
+    public float groundRadius;
+
+    bool facingRight;
+    public bool isGrounded;
+    public bool jump;
+
+    public Transform[] groundPoints;
+
+    public LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = IsGrounded();
+        Move(horizontal);
+        FlipCharacter(horizontal);
+    }
+
+    private void Move(float direction)
+    {
+        rb.velocity = new Vector2(direction * movementSpeed, rb.velocity.y);
+
+        if(isGrounded && jump)
+        {
+            isGrounded = false;
+            jump = false;
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    private void FlipCharacter(float direction)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if(rb.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, ground);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if(colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
