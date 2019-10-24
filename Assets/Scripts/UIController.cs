656b5d7b08b7af7c53e5d9fd9ee14b93273eafce
Shadowpalkia999 <56932812+Shadowpalkia999@users.noreply.github.com>
2019-10-24 13:59:22 -0400

    Added a Level Select scene, a UIController object and script

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("_Level_Select");
+    }
+}
