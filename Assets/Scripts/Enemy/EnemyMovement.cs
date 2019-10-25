e04c01b40ceef86bffaa524ebebd994cdca10bfe
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-25 14:27:44 -0400

    added enemy movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    public int current = 0;
+
+    public float speed;
+    public float waypointRadius = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(waypoints[current].position, transform.position) < waypointRadius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].position, Time.deltaTime * speed);
+    }
+}
