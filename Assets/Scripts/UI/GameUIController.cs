815ad7a011bb7d753773fd024d8619e8f9cb33f9
Shadowpalkia999 <56932812+Shadowpalkia999@users.noreply.github.com>
2019-10-29 17:21:55 -0400

    Worked on the basic elements for the code of the level UI and connected the buttons to their respective methods

--- Assets/Scripts/UI/GameUIController.cs
+++ Assets/Scripts/UI/GameUIController.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject winScreen;
+
+    private int lives;
+
     void Start()
     {
-        
+        winScreen.SetActive(false);
+        lives = 3;
     }
-
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnNextLevelButtonClick()
+    {
+        if (SceneManager.GetActiveScene().name == "_Level_1")
+        {
+            SceneManager.LoadScene("_Level_2");
+        }
+        else if (SceneManager.GetActiveScene().name == "_Level_2")
+        {
+            SceneManager.LoadScene("_Level_3");
+        }
+        else
+        {
+            SceneManager.LoadScene("_level_select");
+        }
+    }
+    public void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("_level_select");
     }
 }
