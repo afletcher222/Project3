43be58658cb18c4368b3f718ea68c892d5795b0d
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-10-30 12:51:17 -0400

    af

---
+++ Assets/Scripts/Jack's Puzzles/SwitchController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private bool switchStateRed;
+    private bool switchStateBlue;
+
+    void Start()
+    {
+        switchStateRed = true;
+        switchStateBlue = false;
+        ActivatePlatforms();
+    }
+    void Update()
+    {
+        
+    }
+    private void SwitchState()
+    {
+        if (switchStateRed == true && switchStateBlue == false)
+        {
+            switchStateRed = false;
+            switchStateBlue = true;
+        }
+        else if (switchStateRed == false && switchStateBlue == true)
+        {
+            switchStateRed = false;
+            switchStateBlue = true;
+        }
+        ActivatePlatforms();
+    }
+    private void ActivatePlatforms()
+    {
+        if (switchStateRed)
+        {
+
+        }
+        else if (switchStateBlue)
+        {
+
+        }
+    }
+}
