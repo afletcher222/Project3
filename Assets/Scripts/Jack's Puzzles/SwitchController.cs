9c5a11137759dfa3036742d4090ecccde18447d2
NyneElevun <56932845+NyneElevun@users.noreply.github.com>
2019-10-30 11:52:46 -0400

    again2

---
+++ Assets/Scripts/Jack's Puzzles/SwitchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private bool switchStateRed;
+    private bool switchStateBlue;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
