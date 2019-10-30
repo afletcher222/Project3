fd45bd8684213c9f743053ef5ea192455b56985c
GabrielNewt <56932831+GabrielNewt@users.noreply.github.com>
2019-10-30 13:33:09 -0400

    Help Menu and Icons

---
+++ Assets/Scripts/ButtonTestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonTestScript : MonoBehaviour
+{
+    public Button CloseButton;
+    public GameObject HelpPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HelpPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void OnCloseButtonClick()
+    {
+        HelpPanel.SetActive(false);
+    }
+}
