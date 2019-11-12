f25bb697c048e9b27a80298088a8a7364686c06d
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-11-12 11:35:27 -0500

    made prefab of second enemy and fixed sound issue

---
+++ Assets/Scripts/Enemy/EnemyMovementWithAnim2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementWithAnim2 : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    public int current = 0;
+
+    public float speed = 7;
+    public float waypointRadius = 1;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(waypoints[current].position, transform.position) < waypointRadius)
+        {
+            current++;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                Quaternion rotate = transform.rotation;
+                rotate.z *= -1;
+                transform.rotation = rotate;
+                transform.localScale = scale;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].position, Time.deltaTime * speed);
+    }
+}
