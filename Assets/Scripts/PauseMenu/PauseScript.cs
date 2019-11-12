37ff5a9a2d27aa70dd5fab5d970b062c9560be20
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-11-12 10:26:16 -0500

    added a hint screen

---
+++ Assets/Scripts/PauseMenu/PauseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject hintMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hintMenu.SetActive(false);
+    }
+
+    public void OnHintButtonClick()
+    {
+        hintMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void OnHintButtonCloseClick()
+    {
+        Time.timeScale = 1;
+        hintMenu.SetActive(false);
+    }
+}
